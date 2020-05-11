0a7855e7ba4d0171533a83a3de41e95d50bae893
Mildred Gil <mi.mitel@hotmail.com>
2020-05-11 08:16:06 -0500

    challenge 2 and 3

--- Coding-Challenge-2-and-3/models/sport-model.js
+++ Coding-Challenge-2-and-3/models/sport-model.js
@@ -1,7 +1,38 @@
 const mongoose = require( 'mongoose' );
 
 /* Your code goes here */
+const sportSchema = mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    num_players: {
+        type: Number,
+        required: true,
+        unique: true
+    }
+})
+
+let SportsCollection = mongoose.model('sports', sportSchema);
+
+Sports = {
+    create: function (id, name, num_players) {
+        return SportsCollection
+        .create({id: id, name: name, num_players: num_players})
+        .then((result) => {
+            return result;
+        }).catch((err) => {
+            return err;
+        });
+    }
+}
 
 module.exports = {
-    
+    Sports
 };
--- Coding-Challenge-2-and-3/server.js
+++ Coding-Challenge-2-and-3/server.js
@@ -3,12 +3,81 @@ const bodyParser = require( 'body-parser' );
 const mongoose = require( 'mongoose' );
 const jsonParser = bodyParser.json();
 const { DATABASE_URL, PORT } = require( './config' );
-
 const app = express();
+const { Sports } = require('./models/sport-model');
+const morgan = require('morgan');
 
-
+app.use(morgan("dev"));
+let sports = [
+    {
+        id: 1,
+        name: "futbol",
+        num_players: 12
+    },
+    {
+        id: 2,
+        name: "basketball",
+        num_players: 5
+    }
+];
+let count = 3;
 /* Your code goes here */
+app.post('/sports/addSport/:sportId', [jsonParser], function (req, res) {
+  let {sportId} = req.params;
+  let {id, name, num_players} = req.body;
+    console.log(id, name, num_players)
+  if( !name || !num_players || !id) {
+    res.statusMessage = "One of this is missing: name, num_players, id";  
+    return res.status(406).end();
+  }
 
+  if(Number(sportId) != Number(id)) {
+    res.statusMessage = "sportId parameter and id in body didnt match";  
+    return res.status(409).end();
+  }
+
+  //add 400
+  let v = false;
+  for (var sport of sports) {
+      if(sport.id === Number(sportId)){
+        v = true;
+        break;
+      }
+  }
+  console.log(v)
+  if(v){
+      res.statusMessage = "sport already exists"
+      return res.status(400).end();
+  }
+
+  let newS = {
+        id: Number(sportId),
+        name: name,
+        num_players: num_players
+    };
+  sports.push(newS);
+    count = count + 1;
+  res.status(201).json(sports);
+})
+
+app.post('/create',[jsonParser], function (req, res) {
+    let {id, name, num_players} = req.body;
+    /*let newS = {
+        "id": Number(id), 
+        "name":name, 
+        "num_players": Number(num_players)}*/
+
+    
+    Sports
+    .create(id, name, num_players)
+    .then((result) => {
+        console.log(result);
+        return res.status(200).json(result);
+    }).catch((err) => {
+        res.statusMessage = "Error database problems, comeback later"
+        return res.status(500).end();
+    });
+})
 
 app.listen( PORT, () => {
     console.log( "This server is running on port 8080" );
